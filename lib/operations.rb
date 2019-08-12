611c815cd2d4c45c873d3778c8bd187a86b389da
Maverick Adolfo <merkonical@gmail.com>
2019-08-12 19:17:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,9 @@
 def unsafe?(speed)
-    speed = 70
-    if speed < 60
-	   "safe"
-    else
-	    "unsafe"
-  end	
+  if speed < 40 || speed > 60
+    return true
+  else 
+    return false
+  end
 end
 
 def not_safe?(speed)
